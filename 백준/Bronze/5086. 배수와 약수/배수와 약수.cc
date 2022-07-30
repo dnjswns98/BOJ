668a662b28466d3926fb905b30837181cda7a632
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2022-07-30 16:13:58 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int a, b;
+    while (1) {
+        cin >> a >> b;
+        if (a == 0 && b == 0) {
+            return 0;
+        }
+        if (b % a == 0) {
+            cout << "factor\n";
+        }
+        else if (a % b == 0) {
+            cout << "multiple\n";
+        }
+        else
+            cout << "neither\n";
+    }
+}
