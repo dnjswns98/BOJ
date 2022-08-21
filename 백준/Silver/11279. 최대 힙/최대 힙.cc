b47fe0d8884272b5b6114d0aeed400bdc63bf1cb
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2022-08-21 16:06:40 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int n;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	int tmp;
+	priority_queue<int> q;
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
+		if (tmp > 0) {
+			q.push(tmp);
+		}
+		else {
+			if (q.empty())
+				cout << "0\n";
+			else {
+				cout << q.top() << "\n";
+				q.pop();
+			}
+		}
+	}
+}
