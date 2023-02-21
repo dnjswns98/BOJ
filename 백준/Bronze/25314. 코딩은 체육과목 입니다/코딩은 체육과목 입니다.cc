c372443f17298bc21d61c71baa2e71da209a4942
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2023-02-21 16:21:44 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	long long n, m, k;
+
+	cin >> n;
+	for (int i = 0; i < n/4; i++) {
+		cout << "long ";
+	}
+	cout << "int";
+}
