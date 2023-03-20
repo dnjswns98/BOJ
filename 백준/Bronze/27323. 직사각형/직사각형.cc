090696e41e0b56825a48880ddd7264dc7645c267
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2023-03-20 23:40:32 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#define max 1000001
+
+int arr[max] = { 0 };
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	int a, b;
+	cin >> a >> b;
+	cout << a * b;
+}
