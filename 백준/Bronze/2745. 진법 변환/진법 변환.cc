518f993b54628748da24f37596d537cd4c2d1c18
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2023-03-31 00:02:11 +0900

    [Bronze II] Title: 진법 변환, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.cc
+++ 백준/Bronze/2745. 진법 변환/진법 변환.cc
@@ -1,28 +1,26 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
 using namespace std;
 
-int main()
-{
-    string b_num;
-    int n;
-    cin >> b_num >> n;
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
 
-    int sum(0);
-    for (int i = 0; i < b_num.length(); i++)
-    {
-        int tmp = b_num[b_num.length() - (i + 1)];
-        if ('0' <= tmp && tmp <= '9')
-        {
-            tmp = tmp - '0';
-        }
-        else
-        {
-            tmp = tmp + 10 - 'A';
-        }
-        sum += (tmp * (int)pow(n, i));
-    }
-    cout << sum << '\n';
+	int b;
+	string s;
+	cin >> s >> b;
+	int size = s.size();
+	int sum = 0;
+	for (int i = size - 1; i >= 0; i--) {
+		int num = s[i];
+		if (num >= 'A') num = num - 'A' + 10;
+		else num = num - '0';
 
-    return 0;
+		int ex = 1;
+		for (int j = i + 1; j < size; j++) {
+			ex *= b;
+		}
+		sum += num * ex;
+	}
+	cout << sum;
 }
