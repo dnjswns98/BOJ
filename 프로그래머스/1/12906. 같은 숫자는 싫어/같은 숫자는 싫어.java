e5077dfc06ff2cf3636e21335462ffb19f5c4ce9
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2024-10-13 17:12:27 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 32.74 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
+    public Stack<Integer> solution(int []arr) {
         Stack<Integer> s = new Stack<>();
         s.push(arr[0]);
         for(int i = 1; i<arr.length; i++){
@@ -9,13 +9,7 @@ public class Solution {
                 s.push(arr[i]);
             }
         }
-        
-        int[] answer = new int[s.size()];
-        // 스택에서 배열로 값을 옮기기 (역순이 아님)
-        for (int i = answer.length - 1; i >= 0; i--) {
-            answer[i] = s.pop();  // 스택의 맨 위 원소를 꺼내서 배열에 저장
-        }
 
-        return answer;
+        return s;
     }
 }
