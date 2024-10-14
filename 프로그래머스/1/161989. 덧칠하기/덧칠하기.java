ec6f1b3f77d11a918195f624450ac25dcb997c93
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2024-10-14 17:31:34 +0900

    [level 1] Title: 덧칠하기, Time: 2.11 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,25 +1,13 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 0;
-        boolean[] chk = new boolean[n + 1];
-        
-        for(int i = 0; i<section.length; i++){
-            //덧칠 할 부분이 칠해져있지 않다면
-            if(!chk[section[i]]){
-                //덧칠 할 시작 부분에서 롤러의 크기가 벽의 범위를 벗어나면
-                if(section[i] + m > n + 1){
-                    for(int j = section[i]; j<=n; j++){
-                        chk[j] = true;
-                    }
-                    answer++;
-                }
-                else{
-                    for(int j = section[i]; j<section[i] + m; j++){
-                        chk[j] = true;
-                    }
-                    answer++;
-                }
+        int answer = 1;
+        int start = section[0];
+        for(int i = 1; i<section.length; i++){
+            if(start + m > section[i]){
+                continue;
             }
+            start = section[i];
+            answer++;
         }
         
         return answer;
