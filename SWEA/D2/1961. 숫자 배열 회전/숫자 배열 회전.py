74418651f9e3731a229d160d81759cc904ca1136
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2024-12-30 21:29:57 +0900

    [D2] Title: 숫자 배열 회전, Time: 118 ms, Memory: 45,056 KB -BaekjoonHub

---
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -0,0 +1,29 @@
+T = int(input())
+
+
+def rotate(arr, n):
+    result = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            result[i][j] = arr[n - 1 - j][i]
+    return result
+
+
+for test_case in range(1, T + 1):
+    n = int(input())
+    arr = list(input().split() for _ in range(n))
+    arr_90 = rotate(arr, n)
+    arr_180 = rotate(arr_90, n)
+    arr_270 = rotate(arr_180, n)
+
+    print("#{} ".format(test_case))
+    for i in range(n):
+        for j in range(n):
+            print(arr_90[i][j], end="")
+        print(end=" ")
+        for j in range(n):
+            print(arr_180[i][j], end="")
+        print(end=" ")
+        for j in range(n):
+            print(arr_270[i][j], end="")
+        print()
