d5830d7121f066032dea63859b000b014b1b4591
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2025-03-06 00:07:12 +0900

    [D5] Title: 현주가 좋아하는 제곱근 놀이, Time: 166 ms, Memory: 33,652 KB -BaekjoonHub

--- SWEA/D5/6782. 현주가 좋아하는 제곱근 놀이/현주가 좋아하는 제곱근 놀이.java
+++ SWEA/D5/6782. 현주가 좋아하는 제곱근 놀이/현주가 좋아하는 제곱근 놀이.java
@@ -1,28 +1,32 @@
-import java.io.*;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
- 
-        int tc = Integer.parseInt(br.readLine());
-        for(int z=1;z<=tc;z++) {
-            // 10^12
-            long n = Long.parseLong(br.readLine());
-            int result = 0;
-            while(n!=2) {
-                double t = Math.sqrt(n);
-                if((int)t == t) {
-                    n = (int)t;
-                }else {
-                    t = (int)t;
-                    result += ((t+1)*(t+1)-n);
-                    n = (int)Math.sqrt((t+1)*(t+1));
-                }
-                result++;
-            }
-            bw.write("#"+z+" "+result+"\n");
-        }
-        bw.flush();
-    }// end of main
-}// end of class
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+
+		for (int test = 1; test <= t; test++) {
+			long n = Long.parseLong(br.readLine());
+			
+			int cnt = 0;
+			while(n != 2) {
+				long temp = (long) Math.sqrt(n);
+				if(temp * temp == n) { //n의 제곱근이 정수인 경우
+					n = temp;
+				}
+				else { // n의 제곱근이 정수가 아니라면
+					//n값에서 +1씩 계속 해주면서 제곱근이 정수가 되는 값을 찾아야한다.
+					temp++;
+					cnt += temp*temp - n; // 제곱근이 정수가 되는 값 - n = +1을 해야하는 횟수
+					n = temp;
+				}
+				cnt++;
+			}
+			
+			System.out.println("#" + test + " " + cnt);
+		}
+	}
+}
