5fef255bf68ea9e23bb3551908904243ba32adfd
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2025-04-03 15:33:04 +0900

    [Gold IV] Title: 알고스팟, Time: 88 ms, Memory: 12500 KB -BaekjoonHub

---
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static class Point implements Comparable<Point> {
+		int x;
+		int y;
+		int cost;
+
+		public Point(int x, int y, int cost) {
+			this.x = x;
+			this.y = y;
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Point o) {
+			return Integer.compare(this.cost, o.cost);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+
+		int[][] arr = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			char[] c = br.readLine().toCharArray();
+			for (int j = 0; j < m; j++) {
+				arr[i][j] = c[j] - '0';
+			}
+		}
+
+		int[][] minDist = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			Arrays.fill(minDist[i], Integer.MAX_VALUE);
+		}
+		minDist[0][0] = arr[0][0];
+
+		PriorityQueue<Point> pq = new PriorityQueue<>();
+		pq.add(new Point(0, 0, 0));
+
+		int[] dx = { -1, 1, 0, 0 };
+		int[] dy = { 0, 0, -1, 1 };
+
+		while (!pq.isEmpty()) {
+			Point cur = pq.poll();
+
+			if (cur.x == n - 1 && cur.y == m - 1)
+				break;
+
+			for (int i = 0; i < 4; i++) {
+				int nx = cur.x + dx[i];
+				int ny = cur.y + dy[i];
+
+				if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+
+				if (minDist[nx][ny] > cur.cost + arr[nx][ny]) {
+					minDist[nx][ny] = cur.cost + arr[nx][ny];
+					pq.add(new Point(nx, ny, minDist[nx][ny]));
+				}
+			}
+		}
+
+		System.out.println(minDist[n - 1][m - 1]);
+	}
+}
