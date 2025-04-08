08dc99f68d74046c42adcefd8ef4d9deb430f8ab
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2025-04-08 14:16:13 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 72 ms, Memory: 11992 KB -BaekjoonHub

--- 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
@@ -5,44 +5,39 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int n;
-	static int[][] arr;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
+		StringTokenizer st;
 
-		arr = new int[n][n];
+		int n = Integer.parseInt(br.readLine());
+		int[][] arr = new int[n][n];
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < n; j++) {
+			for(int j = 0; j<n; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		// 0 : 가로, 1 : 세로, 2 : 대각선
+		
 		long[][][] dp = new long[n][n][3];
 		for(int i = 1; i<n; i++) {
-			if(arr[0][i] == 1) break; // 중간에 벽 있으면 멈춤
-			dp[0][i][0] = 1; // 초기값 : 파이프의 오른쪽 끝이면서 방향은 가로
+			if(arr[0][i] == 1) break; // 벽 마주치면 끝
+			dp[0][i][0] = 1; // 초기화
 		}
-
-		// 파이프는 우/우하/하 로만 이동이 가능하므로 2열~n열까지만 움직임
-		for (int i = 1; i < n; i++) {
-			for (int j = 2; j < n; j++) {
+		
+		for(int i = 1; i<n; i++) {
+			for(int j = 2; j<n; j++) {
 				if(arr[i][j] == 1) continue;
 				
-				// 가로 : 이전이 가로 or 이전이 대각선
+				// 가로 : 이전이 가로 or 대각선
 				dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
 				
-				// 세로 : 이전이 세로 or 이전이 대각선
+				// 세로 : 이전이 세로 or 대각선
 				dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
-						
-				// 대각선 : 세 칸이 모두 비어 있어야 가능
-				// 모든 방향에서 갈 수 있음
+				
+				// 대각선 : 모든 방향 가능
+				// 인접칸 빈 칸 체크
 				if(arr[i - 1][j] == 0 && arr[i][j - 1] == 0) {
-					dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1]  + dp[i - 1][j - 1][2];
+					dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
 				}
 			}
 		}
