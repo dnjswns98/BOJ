1869045d0cf93442e9147c7dc3161d2dd3a9961c
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2025-07-16 10:17:07 +0900

    [Gold V] Title: 용액, Time: 276 ms, Memory: 31280 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int n;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int left = 0;
+		int right = n - 1;
+		int min = Integer.MAX_VALUE;
+		int[] ans = new int[2];
+		while(left < right) {
+			int sum = arr[left] + arr[right];
+			
+			if(Math.abs(sum) < min) {
+				min = Math.abs(sum);
+				ans[0] = arr[left];
+				ans[1] = arr[right];
+			}
+			
+			if(sum < 0) {
+				left++;
+			}
+			else right--;
+		}
+		
+		System.out.println(ans[0] + " " + ans[1]);
+	}
+}
