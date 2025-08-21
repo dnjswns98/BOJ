c0a1251b4fb59e2a0189456d28e10c35de66c20c
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2025-08-21 12:02:32 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 596 ms, Memory: 67740 KB -BaekjoonHub

---
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -0,0 +1,99 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int n, m;
+	static ArrayList<Node>[] list;
+	static final int INF = 1000000000;
+	
+	static class Node implements Comparable<Node> {
+		int v;
+		int w;
+		
+		public Node(int v, int w) {
+			super();
+			this.v = v;
+			this.w = w;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.w, o.w);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		list = new ArrayList[n + 1];
+		for(int i = 1; i<=n; i++) {
+			list[i] = new ArrayList<>();
+		}
+		
+		for(int i = 0; i<m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			
+			list[from].add(new Node(to, w));
+			list[to].add(new Node(from, w));
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		int v1 = Integer.parseInt(st.nextToken());
+		int v2 = Integer.parseInt(st.nextToken());
+		
+		int[] dist = dijkstra(1);
+		int sToV1 = dist[v1];
+		int sToV2 = dist[v2];
+		
+		dist = dijkstra(v1);
+		int v1ToV2 = dist[v2];
+		int v1ToN = dist[n];
+		
+		dist = dijkstra(v2);
+		int v2ToN = dist[n];
+		
+		int result = Math.min(sToV1 + v1ToV2 + v2ToN, sToV2 + v1ToV2 + v1ToN);
+		
+		if(v1ToV2 == INF || result >= INF)
+			System.out.println(-1);
+		else System.out.println(result);
+
+	}
+	
+	static int[] dijkstra(int start) {
+		int[] minDist = new int[n + 1];
+		Arrays.fill(minDist, INF);
+		minDist[start] = 0;
+		
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.offer(new Node(start, 0));
+		
+		while(!pq.isEmpty()) {
+			Node cur = pq.poll();
+			
+			if(minDist[cur.v] < cur.w) continue;
+			
+			for(Node next : list[cur.v]) {
+				if(minDist[next.v] > minDist[cur.v] + next.w) {
+					minDist[next.v] = minDist[cur.v] + next.w;
+					pq.offer(new Node(next.v, minDist[next.v]));
+				}
+			}
+		}
+		
+		return minDist;
+	}
+}
