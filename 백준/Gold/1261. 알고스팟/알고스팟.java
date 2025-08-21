c594c8cbaaf499d0bc0e7a949dc26ecb13ce7aaa
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2025-08-21 12:48:34 +0900

    [Gold IV] Title: 알고스팟, Time: 84 ms, Memory: 12344 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,74 +1,79 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static class Point implements Comparable<Point> {
+	static int n, m;
+	
+	static class Point implements Comparable<Point>{
 		int x;
 		int y;
-		int cost;
+		int cnt;
 
-		public Point(int x, int y, int cost) {
+		public Point(int x, int y, int cnt) {
 			this.x = x;
 			this.y = y;
-			this.cost = cost;
+			this.cnt = cnt;
 		}
 
 		@Override
 		public int compareTo(Point o) {
-			return Integer.compare(this.cost, o.cost);
+			return Integer.compare(this.cnt, o.cnt);
 		}
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int m = Integer.parseInt(st.nextToken());
-		int n = Integer.parseInt(st.nextToken());
+
+		m = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
 
 		int[][] arr = new int[n][m];
 		for (int i = 0; i < n; i++) {
 			char[] c = br.readLine().toCharArray();
-			for (int j = 0; j < m; j++) {
+			for(int j = 0; j<m; j++) {
 				arr[i][j] = c[j] - '0';
 			}
 		}
 
-		int[][] minDist = new int[n][m];
-		for (int i = 0; i < n; i++) {
-			Arrays.fill(minDist[i], Integer.MAX_VALUE);
-		}
-		minDist[0][0] = arr[0][0];
-
-		PriorityQueue<Point> pq = new PriorityQueue<>();
-		pq.add(new Point(0, 0, 0));
-
 		int[] dx = { -1, 1, 0, 0 };
-		int[] dy = { 0, 0, -1, 1 };
-
-		while (!pq.isEmpty()) {
-			Point cur = pq.poll();
-
-			if (cur.x == n - 1 && cur.y == m - 1)
-				break;
-
-			for (int i = 0; i < 4; i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
-
-				if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-
-				if (minDist[nx][ny] > cur.cost + arr[nx][ny]) {
-					minDist[nx][ny] = cur.cost + arr[nx][ny];
-					pq.add(new Point(nx, ny, minDist[nx][ny]));
+		int[] dy = { 0, 0, 1, -1 };
+		
+		int INF = Integer.MAX_VALUE;
+		int[][] minDist = new int[n][m];
+		for(int i = 0; i<n; i++) {
+			Arrays.fill(minDist[i], INF);
+		}
+		minDist[0][0] = 0;
+		
+		PriorityQueue<Point> pq = new PriorityQueue<>();
+		pq.offer(new Point(0, 0, 0));
+		while(!pq.isEmpty()) {
+			Point p = pq.poll();
+			
+			if(p.x == n - 1 && p.y == m - 1) {
+				System.out.println(p.cnt);
+				return;
+			}
+			
+			for(int i = 0; i<4; i++) {
+				int nx = p.x + dx[i];
+				int ny = p.y + dy[i];
+				
+				if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+				
+				if(minDist[nx][ny] > p.cnt + arr[nx][ny]) {
+					minDist[nx][ny] = p.cnt + arr[nx][ny];
+					pq.offer(new Point(nx,ny,minDist[nx][ny]));
 				}
 			}
 		}
-
-		System.out.println(minDist[n - 1][m - 1]);
 	}
 }
