a415608ae15723fe6eda10eff592ed53b10a3432
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2025-08-22 10:18:27 +0900

    [Gold II] Title: 친구 네트워크, Time: 1932 ms, Memory: 91220 KB -BaekjoonHub

---
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static HashMap<String, String> parents;
+	static HashMap<String, Integer> size;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int t = Integer.parseInt(br.readLine());
+		for (int test = 0; test < t; test++) {
+			int n = Integer.parseInt(br.readLine());
+
+			parents = new HashMap<>();
+			size = new HashMap<>();
+
+			for (int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
+				String a = st.nextToken();
+				String b = st.nextToken();
+
+				if (!parents.containsKey(a)) {
+					parents.put(a, a);
+					size.put(a, 1);
+				}
+				if (!parents.containsKey(b)) {
+					parents.put(b, b);
+					size.put(b, 1);
+				}
+
+				union(a, b);
+			}
+		}
+	}
+
+	static String find(String x) {
+		if (x == parents.get(x))
+			return x;
+		parents.put(x, find(parents.get(x)));
+		return parents.get(x);
+	}
+
+	static void union(String a, String b) {
+		String aRoot = find(a);
+		String bRoot = find(b);
+
+		if (!aRoot.equals(bRoot)) {
+			parents.put(bRoot, aRoot);
+
+			int aSize = size.get(aRoot);
+			int bSize = size.get(bRoot);
+			size.put(aRoot, aSize + bSize);
+		}
+		
+		System.out.println(size.get(aRoot));
+	}
+}
