7be634ce929deed239eb21753809c87974bdcd02
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2025-11-22 01:51:41 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.31 ms, Memory: 61.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 1; i<food.length; i++) {
+            if(food[i] / 2 >= 1) {
+                for(int j = 1; j <= food[i] / 2; j++) {
+                    sb.append(i);
+                }
+            }
+        }
+
+        String answer = sb.reverse().toString();
+        sb.reverse();
+        sb.append(0).append(answer);
+        return sb.toString();
+    }
+}
