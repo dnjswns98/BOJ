023d3682acb1847130283d8cadb35f505f9c192c
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2025-11-24 00:28:30 +0900

    [level 2] Title: 할인 행사, Time: 50.03 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=java) 
 
 ### 성능 요약
 
-메모리: 112 MB, 시간: 33.97 ms
+메모리: 120 MB, 시간: 50.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 13일 18:18:42
+2025년 11월 24일 00:28:29
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -4,39 +4,37 @@ class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> wanted = new HashMap<>();
         for(int i = 0; i<want.length; i++) {
-            map.put(want[i],number[i]);
+            wanted.put(want[i], number[i]);
         }
         
         for(int i = 0; i<10; i++) {
-            if(map.containsKey(discount[i])) {
-                map.put(discount[i], map.get(discount[i]) - 1);
+            if(wanted.containsKey(discount[i])) {
+                wanted.put(discount[i], wanted.get(discount[i]) - 1);
             }
         }
         
-        if(isMatch(map))
-            answer++;
+        if(isPossible(wanted)) answer++;
         
-        for(int i = 10; i<discount.length; i++) {
-            if(map.containsKey(discount[i - 10])) {
-                map.put(discount[i - 10], map.get(discount[i - 10]) + 1);
-            }
-            if(map.containsKey(discount[i])) {
-                map.put(discount[i], map.get(discount[i]) - 1);
+        for(int i = 10; i < discount.length; i++) {
+            if(wanted.containsKey(discount[i - 10])) {
+                wanted.put(discount[i - 10], wanted.get(discount[i - 10]) + 1);
             }
             
-            if(isMatch(map))
-                answer++;
+            if(wanted.containsKey(discount[i])) {
+                wanted.put(discount[i], wanted.get(discount[i]) - 1);
+            }
+            
+            if(isPossible(wanted)) answer++;
         }
         
         return answer;
     }
     
-    public boolean isMatch(HashMap<String, Integer> map) {
-        for(String key : map.keySet()) {
-            if(map.get(key) != 0)
-                return false;
+    public boolean isPossible(HashMap<String, Integer> wanted) {
+        for(String key : wanted.keySet()) {
+            if(wanted.get(key) != 0) return false;
         }
         return true;
     }
