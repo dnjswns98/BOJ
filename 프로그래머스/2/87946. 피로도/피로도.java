4da8f13cf5b6f7c0d82c72e100b62142a3dea4c6
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2026-03-23 14:58:03 +0900

    [level 2] Title: 피로도, Time: 3.19 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,25 +1,20 @@
 import java.util.*;
 
 class Solution {
-    
-    boolean[] visited;
     int answer;
     
     public int solution(int k, int[][] dungeons) {     
-        visited = new boolean[dungeons.length];
         answer = 0;
-        dfs(0, k, dungeons);
+        dfs(0, k, dungeons, 0);
         
         return answer;
     }
     
-    void dfs(int cnt, int fatigue, int[][] dungeons) { 
+    void dfs(int cnt, int fatigue, int[][] dungeons, int visited) { 
         for(int i = 0; i<dungeons.length; i++) {
             // 방문하지 않았고, 필요도가 가능할때
-            if(!visited[i] && dungeons[i][0] <= fatigue) {
-                visited[i] = true;
-                dfs(cnt + 1, fatigue - dungeons[i][1], dungeons);
-                visited[i] = false;
+            if((visited & (1 << i)) == 0 && dungeons[i][0] <= fatigue) {
+                dfs(cnt + 1, fatigue - dungeons[i][1], dungeons, visited | (1 << i));
             }
         }
         
