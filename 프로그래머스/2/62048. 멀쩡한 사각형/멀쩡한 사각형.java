dd063ac90846dd12c2ec8de03fe2df0a1777b43b
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2026-05-18 23:58:48 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.04 ms, Memory: 64.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long solution(int w, int h) {
+        
+        long W = w;
+        long H = h;
+        
+        return W * H - (W + H - gcd(W, H));
+    }
+    
+    public long gcd(long a, long b) {
+        if(b == 0) return a;
+        return gcd(b, a % b);
+    }
+}
