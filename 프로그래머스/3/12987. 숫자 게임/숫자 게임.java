12f9a394f463d8968e0aad9c4beaebf001fde0aa
dnjswns98 <108859978+dnjswns98@users.noreply.github.com>
2026-08-07 22:40:57 +0900

    [level 3] Title: 숫자 게임, Time: 36.80 ms, Memory: 68.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int i = 0, j = 0;
+        while(i < A.length && j < B.length) {
+            if(A[i] < B[j]) {
+                answer++;
+                i++;
+                j++;
+            }
+            else {
+                j++;
+            }
+        }
+        
+        return answer;
+    }
+}
